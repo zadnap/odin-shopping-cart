c1daec80caa7ed3bf4b79934ec5421fb31ed11a1
zadnap <zadnap@users.noreply.github.com>
2025-06-18 15:47:14 +0700

    feat: initialize project with neccessary packages
    This project is powered with react-vite template. This template includes
    react, react-router-dom, vitest, React Testing Library, sass and
    fontawesome.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import styles from './App.module.scss';
+
+function App() {
+  return <div className={styles.app}></div>;
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './testSetup.js',
+  },
+});
