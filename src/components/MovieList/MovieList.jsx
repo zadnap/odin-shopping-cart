3c3e8597fc9654814d87c53795408c70e5168dcc
zadnap <zadnap@users.noreply.github.com>
2025-06-23 09:15:12 +0700

    fix(scss): change the styles of components
    When I combine every component I have created so far, there are some
    style error on screen, so I modify scss and add some variables so that
    they look good together.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,33 @@
-import MovieCard from '../MovieCard/MovieCard';
+import MovieCard from '@/components/MovieCard/MovieCard';
+import Button from '@/components/Button/Button';
 import styles from './MovieList.module.scss';
 
 function MovieList({ movies }) {
   return (
     <section className={styles.movieList}>
+      <ul className={styles.tagList}>
+        <li className={styles.tag}>
+          <Button>Trending</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Adventure</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Action</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Comedy</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Crime</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Drama</Button>
+        </li>
+        <li className={styles.tag}>
+          <Button>Fantasy</Button>
+        </li>
+      </ul>
       <ul className={styles.listGrid}>
         {movies.map((movie) => (
           <li key={movie.id} className={styles.movieItem}>
