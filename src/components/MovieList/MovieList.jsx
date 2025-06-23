229cfa55d848a7c3896d2c24d4e17d3cf6593fd6
zadnap <zadnap@users.noreply.github.com>
2025-06-23 17:07:56 +0700

    feat(ui): add genre filter section to MovieList
    This part is a small section of the MovieList component, which helps
    user to navigate to find the genre they want to choose. User can use
    mouse to scroll horizontally or use the navigation button on the right.
    
    Besides, since I use position relative for the genre filter, I have to
    change the index of the header so that it floats on top of everything
    else including the genre filter.

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,33 +1,66 @@
 import MovieCard from '@/components/MovieCard/MovieCard';
 import Button from '@/components/Button/Button';
 import styles from './MovieList.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 function MovieList({ movies }) {
   return (
     <section className={styles.movieList}>
-      <ul className={styles.tagList}>
-        <li className={styles.tag}>
-          <Button>Trending</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Adventure</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Action</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Comedy</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Crime</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Drama</Button>
-        </li>
-        <li className={styles.tag}>
-          <Button>Fantasy</Button>
-        </li>
-      </ul>
+      <div className={styles.genreFilter}>
+        <ul className={styles.tagList}>
+          <li className={styles.tag}>
+            <Button>Trending</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Adventure</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Action</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Comedy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Crime</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Drama</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+          <li className={styles.tag}>
+            <Button>Fantasy</Button>
+          </li>
+        </ul>
+        <div className={styles.navigator}>
+          <Button>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </Button>
+          <Button>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </Button>
+        </div>
+      </div>
       <ul className={styles.listGrid}>
         {movies.map((movie) => (
           <li key={movie.id} className={styles.movieItem}>
