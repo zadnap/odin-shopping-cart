576ed784fed5fefc4bd744d2840f7c77b4a6f768
zadnap <zadnap@users.noreply.github.com>
2025-06-30 09:47:57 +0700

    feat(ui): add App.jsx layout styling
    This layout will be used for most of pages of this wabsite. At the
    moment, since I have not built a route system, I have to use
    MemoryRouter instead of BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import styles from './App.module.scss';
+import { MemoryRouter } from 'react-router-dom';
+import Header from '@/components/Header/Header';
+import Navigation from '@/components/Navigation/Navigation';
 
 function App() {
-  return <div className={styles.app}></div>;
+  return (
+    <div className={styles.app}>
+      <MemoryRouter>
+        <Header />
+        <div className={styles.body}>
+          <Navigation />
+          <div className={styles.outlet}></div>
+        </div>
+      </MemoryRouter>
+    </div>
+  );
 }
 
 export default App;
