7e4df8c88e158963510e66bfa85704498236faf1
zadnap <zadnap@users.noreply.github.com>
2025-07-05 16:47:18 +0700

    feat(TrailerPreview): add Modal and refactor tests
    Since this component is now composed of Modal and VideoPlayer component,
    I have to change its test from snapshot testing to normal
    assertion-based testing so that I can test the behaviour of opening the
    modal when clicking the preview.

--- src/components/TrailerPreview/TrailerPreview.jsx
+++ src/components/TrailerPreview/TrailerPreview.jsx
@@ -1,32 +1,56 @@
 import styles from './TrailerPreview.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faStar } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Modal from '@/components/Modal/Modal';
+import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
 
-function TrailerPreview({ id, title, year, rating, backdropSrc }) {
+function TrailerPreview({ trailerKey, title, year, rating, backdropSrc }) {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
-    <Link
-      to={`/movies/${id}`}
-      className={styles.trailerPreview}
-      style={{ backgroundImage: `url(${backdropSrc})` }}
-    >
-      <h3 className={styles.title}>{title}</h3>
-      <div className={styles.info}>
-        <p className={styles.year} aria-label="Year">
-          {year}
-        </p>
-        <p className={styles.rating} aria-label="Rating">
-          <FontAwesomeIcon icon={faStar} data-testid="star-icon" /> {rating}
-        </p>
-      </div>
-      <div className={styles.playBtn}>
-        <FontAwesomeIcon
-          icon={faPlay}
-          className={styles.icon}
-          data-testid="play-icon"
-        />
-      </div>
-    </Link>
+    <>
+      <button
+        className={styles.trailerPreview}
+        style={{ backgroundImage: `url(${backdropSrc})` }}
+        onClick={() => setIsOpenModal(true)}
+        aria-labelledby="trailer-preview"
+      >
+        <h3 className={styles.title} id="trailer-preview">
+          {title}
+        </h3>
+        <div className={styles.info}>
+          <p className={styles.year}>
+            <span className="sr-only">Year: </span>
+            {year}
+          </p>
+          <p className={styles.rating}>
+            <span className="sr-only">Rating: </span>
+            <FontAwesomeIcon
+              icon={faStar}
+              aria-hidden="true"
+              data-testid="star-icon"
+            />{' '}
+            {rating}
+          </p>
+        </div>
+        <div className={styles.playBtn}>
+          <FontAwesomeIcon
+            icon={faPlay}
+            className={styles.icon}
+            data-testid="play-icon"
+          />
+        </div>
+      </button>
+      {isOpenModal && (
+        <Modal
+          title={`${title}'s trailer`}
+          onClose={() => setIsOpenModal(false)}
+        >
+          <VideoPlayer videoKey={trailerKey} movieTitle={title} />
+        </Modal>
+      )}
+    </>
   );
 }
 
--- src/components/TrailerPreview/TrailerPreview.test.jsx
+++ src/components/TrailerPreview/TrailerPreview.test.jsx
@@ -1,11 +1,13 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { vi, describe, it, expect, beforeEach, beforeAll } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import TrailerPreview from './TrailerPreview';
 
 const mockProps = {
-  id: 1,
+  trailerKey: 'abc-123',
   title: 'League of Legends: Arcane',
+  year: 2025,
   duration: '2:45',
   rating: 9.0,
   backdropSrc:
@@ -13,13 +15,45 @@ const mockProps = {
 };
 
 describe('TrailerPreview component', () => {
-  it('should render to match snapshot', () => {
-    const { container } = render(
+  beforeAll(() => {
+    vi.mock('@/components/Modal/Modal', () => ({
+      default: ({ children }) => <div role="presentation">{children}</div>,
+    }));
+
+    vi.mock('@/components/VideoPlayer/VideoPlayer', () => ({
+      default: ({ movieTitle }) => <iframe title={`${movieTitle}'s trailer`} />,
+    }));
+  });
+
+  beforeEach(() => {
+    render(
       <MemoryRouter>
         <TrailerPreview {...mockProps} />
       </MemoryRouter>
     );
+  });
 
-    expect(container).toMatchSnapshot();
+  it('should render with correct content', () => {
+    const title = screen.getByRole('heading', { name: mockProps.title });
+    const year = screen.getByText(mockProps.year);
+    const rating = screen.getByText(mockProps.rating);
+    const starIcon = screen.getByTestId('star-icon');
+    const playIcon = screen.getByTestId('play-icon');
+
+    expect(title).toBeInTheDocument();
+    expect(year).toBeInTheDocument();
+    expect(rating).toBeInTheDocument();
+    expect(starIcon).toBeInTheDocument();
+    expect(playIcon).toBeInTheDocument();
+  });
+
+  it('should open modal when clicking the trailer preview', async () => {
+    const user = userEvent.setup();
+    const trailerPreview = screen.getByRole('button');
+
+    await user.click(trailerPreview);
+
+    const iframe = screen.getByTitle(`${mockProps.title}'s trailer`);
+    expect(iframe).toBeInTheDocument();
   });
 });
