993504cc7c2850402a3ee2e0a858d56cdfc344ee
zadnap <zadnap@users.noreply.github.com>
2025-07-09 09:45:39 +0700

    fix(ErrorMessage): change the image file name

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,10 +1,10 @@
 import styles from './ErrorMessage.module.scss';
-import errorIllustration from '@/assets/error-illustration.png';
+import errorFace from '@/assets/error-face.png';
 
 function ErrorMessage({ message }) {
   return (
     <div role="alert" className={styles.errorMessage}>
-      <img className={styles.errorImage} src={errorIllustration} alt="" />
+      <img className={styles.errorImage} src={errorFace} alt="" />
       <p className={styles.message}>{message}</p>
     </div>
   );
