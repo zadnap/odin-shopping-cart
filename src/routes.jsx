307651ca4ec5c25356762caf9cd42b22251f2390
zadnap <zadnap@users.noreply.github.com>
2025-08-04 11:43:04 +0700

    chore(config): add route configuration

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,10 @@
 import App from './App';
 import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import Movie from './pages/Movie/Movie';
+import Trending from './pages/Trending/Trending';
+import Upcoming from './pages/Upcoming/Upcoming';
+import Trailers from './pages/Trailers/Trailers';
 
 const routes = [
   {
@@ -9,6 +14,26 @@ const routes = [
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/movie/:id',
+        element: <Movie />,
+      },
+      {
+        path: `/trailers`,
+        element: <Trailers />,
+      },
+      {
+        path: '/upcoming',
+        element: <Upcoming />,
+      },
+      {
+        path: '/trending',
+        element: <Trending />,
       },
     ],
   },
